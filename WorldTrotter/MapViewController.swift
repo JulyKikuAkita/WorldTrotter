dbd42429fd4b8dd384a68fb3e58f982f6b0d6bc6
JulyKikuAkita <i-fang.lee@grindr.com>
2019-03-03 23:28:58 -0800

    wip-draw constraint crash
    update gitignore

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -11,6 +11,8 @@ import MapKit
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     var mapView: MKMapView!
     var locationManager: CLLocationManager!
+    //keeps track of current pin index:
+    var selectedAnnotationIndex: Int = -1
     
     override func loadView() {
         mapView = MKMapView()
@@ -30,6 +32,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
         initLocalizationButton(segmentedControl)
+        intitMapPins(segmentedControl)
 
         let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
         let margin = view.layoutMarginsGuide
@@ -92,9 +95,69 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         mapView.showsUserLocation = true
     }
     
+    @objc func intitMapPins(_ anyView: UIView!) {
+        //create array of location objects:
+        var locations = [Locations]()
+        locations.append(Locations(name: "New York City", lat: 40.730872, long: -74.003066))
+        locations.append(Locations(name: "London", lat: 51.5074, long: 0.1278))
+        locations.append(Locations(name: "Tokyo", lat: 35.6895, long: 139.6917))
+        
+        //drop location pins onto map:
+        for location in locations {
+            let dropPin = MKPointAnnotation()
+            dropPin.coordinate = CLLocationCoordinate2DMake(location.lat, location.long)
+            dropPin.title = location.name
+            mapView.addAnnotation(dropPin)
+        }
+
+        //create button to toggle pins:
+        let pinButton = UIButton(type: .system)
+        pinButton.setTitle("Pins", for: .normal)
+        pinButton.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        pinButton.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        pinButton.translatesAutoresizingMaskIntoConstraints = false
+        pinButton.addTarget(self, action: #selector(selectPin(_:)), for: .touchUpInside)
+        
+        //NSGenericException
+        //        pinButton.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -8).isActive = true
+        //        pinButton.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor).isActive = true
+
+    }
+    
+    @objc func selectPin(_ button: UIButton) {
+        //data checks:
+        if !(mapView.annotations.count > 0) {
+            return
+        }
+        
+        //go to next annotation or back to start if last one:
+        selectedAnnotationIndex += 1
+        if selectedAnnotationIndex >= mapView.annotations.count {
+            selectedAnnotationIndex = 0
+        }
+        
+        //select pin and animate map:
+        let annotation = mapView.annotations[selectedAnnotationIndex]
+        let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 5000, 5000)
+        mapView.setRegion(zoomedInCurrentLocation, animated: true)
+    }
+    
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         //This is a method from MKMapViewDelegate, fires up when the user`s location changes
         let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 2500, 2500)
         mapView.setRegion(zoomedInCurrentLocation, animated: true)
     }
 }
+
+struct Locations {
+    var name: String
+    var lat: Double
+    var long: Double
+    
+    init(name: String, lat: Double, long: Double) {
+        self.name = name
+        self.lat = lat
+        self.long = long
+    }
+}
+
